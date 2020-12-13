8b439228ead50ffb2b90f369b4af425c98c0e4d4
Han Chiang <yaphc@hotmail.com>
2020-12-13 20:35:19 +0800

    Set up mysql with typeorm.

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,13 @@ dotenv.config();
 
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
+  mysqlHost: process.env.MYSQL_HOST,
+  mysqlDb: process.env.MYSQL_DB,
+  mysqlUser: process.env.MYSQL_USER,
+  mysqlPassword: process.env.MYSQL_PASSWORD,
+  mysqlPort: parseInt(process.env.MYSQL_PORT) || 3306,
+  redisCacheUrl: process.env['REDIS_CACHE_URL'],
+  redisKeysUrl: process.env['REDIS_KEYS_URL'],
 };
 
 export default config;
