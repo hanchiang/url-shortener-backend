53d2689879430cd5f7c79572e1331a22d2588802
Han Chiang <yaphc@hotmail.com>
2020-12-18 21:52:00 +0800

    Replace mysql with postgres.

--- src/db/mysql/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `url` (
-  `id` varchar(16) CHARACTER SET utf8 NOT NULL COMMENT 'Unique hash of each URL key',
-  `original_url` varchar(1024) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `expire_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
--- src/db/redis/index.ts
+++ src/db/redis/index.ts
@@ -6,20 +6,14 @@ Promise.promisifyAll(redis)
 
 export class Redis {
   private static instance: Redis;
-  private static readonly REDIS_CACHE = 'REDIS_CACHE'; // redis used for caching shortened url -> original url
-  private static readonly REDIS_KEYS = 'REDIS_KEYS'; // redis used for storing available keys
-  private connection: { [key: string]: RedisClient };
-  private constructor(conn: { [key: string]: RedisClient }) {
-    this.connection = conn;
+  private conn: RedisClient
+  private constructor(redis: RedisClient) {
+    this.conn = redis;
   }
   public static getInstance(): Redis {
     if (!this.instance) {
-      const redisCache = this.connect({ url: config.redisCacheUrl });
-      const redisKeys = this.connect({ url: config.redisKeysUrl });
-      this.instance = new Redis({
-        [this.REDIS_CACHE]: redisCache,
-        [this.REDIS_KEYS]: redisKeys,
-      });
+      const redis = this.connect({ url: config.redisUrl });
+      this.instance = new Redis(redis);
     }
     return this.instance;
   }
@@ -29,18 +23,10 @@ export class Redis {
   }
 
   public static close(): void {
-    for (const conn of Object.values(this.instance.connection)) {
-      conn.quit();
-    }
+    this.instance.conn.quit();
   }
 
-  public static flush(connectionKey?: string): void {
-    if (connectionKey) {
-      this.instance.connection[connectionKey].flushdb();
-    } else {
-      for (const conn of Object.values(this.instance.connection)) {
-        conn.flushdb();
-      }
-    }
+  public static flush(): void {
+    this.instance.conn.flushdb();
   }
 }
