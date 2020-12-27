9a34f1611fabd4580ddf836964184cabe9a2ad69
Han Chiang <yaphc@hotmail.com>
2020-12-27 22:00:32 +0800

    Test

--- test/unit/services/urlShortener.ts
+++ test/unit/services/urlShortener.ts
@@ -3,8 +3,26 @@ import { UrlShortenerServiceImpl } from '../../../src/services/impl/urlShortener
 
 describe('UrlShortenerService unit tests', () => {
   it('Should shorten url with alias if it does not already exist', async () => {
-    const url = 'abc';
+    const url = 'www.google.com';
+    const alias = 'abc';
     const urlShortenerService = new UrlShortenerServiceImpl();
-    await urlShortenerService.shortenUrl(url);
+    await urlShortenerService.shortenUrl(url, alias);
+  });
+
+  it('Should throw error if alias already exist', async () => {
+    const url = 'www.google.com';
+    const alias = 'abc';
+    const urlShortenerService = new UrlShortenerServiceImpl();
+    await urlShortenerService.shortenUrl(url, alias);
+
+    let error;
+    try {
+      await urlShortenerService.shortenUrl(url, alias);
+    } catch(e) {
+      error = e;
+    }
+    expect(error).to.not.be.undefined;
+    expect(error.status).equal(409);
+    expect(error.message).equal(`Alias ${alias} is already taken. Please use another alias`)
   });
 });
