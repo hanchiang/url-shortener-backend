82dffb74de6e0b3abef91274b2b56c6814102a22
han chiang <yaphc@hotmail.com>
2022-05-28 16:35:18 +0800

    Use UrlDao and redis in UrlShortener service.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -6,20 +6,21 @@ import {
 import { ShortenUrlRequest } from './types';
 import { ShortenUrlSchema } from '../controllers/validators';
 import { UrlShortenerServiceImpl } from '../services/impl/urlShortener';
+import { Redis } from '../db/redis';
 
 export const shortenUrl = async (
   req: ValidatedRequest<ShortenUrlSchema>,
   res: Response
 ) => {
   const { url, alias }: ShortenUrlRequest = req.body;
-  const shortenUrlService = new UrlShortenerServiceImpl();
+  const shortenUrlService = new UrlShortenerServiceImpl(Redis.getInstance());
   const shortenedUrl = await shortenUrlService.shortenUrl(url, alias);
   res.json(shortenedUrl);
 };
 
 export const redirectUrl = async (req: Request, res: Response) => {
   const { urlKey } = req.params;
-  const shortenUrlService = new UrlShortenerServiceImpl();
+  const shortenUrlService = new UrlShortenerServiceImpl(Redis.getInstance());
   const originalUrl = await shortenUrlService.getOriginalUrl(urlKey);
   res.redirect(originalUrl);
 };
--- src/services/impl/urlShortener.ts
+++ src/services/impl/urlShortener.ts
@@ -2,15 +2,21 @@ import { UrlShortenerService } from '../urlShortener';
 import { KeyGenerationServiceImpl } from './keyGeneration';
 import { KeyGeneration } from '../keyGeneration';
 import config from '../../config';
-import { Url } from '../../models/url';
 import { throwError, ErrorCode } from '../../utils/error';
 import { urlSafe } from '../../utils/urlSafe';
+import { MemoryStore } from '../../db/redis';
+import { UrlDao, UrlDaoImpl } from '../../db/postgres/dao/urlDao';
 
 export class UrlShortenerServiceImpl implements UrlShortenerService {
   private keyGenerationService: KeyGeneration;
-  constructor() {
+  private urlDao: UrlDao;
+
+  constructor(private redis: MemoryStore) {
+    // TODO:
     this.keyGenerationService = new KeyGenerationServiceImpl();
+    this.urlDao = new UrlDaoImpl();
   }
+
   public async shortenUrl(
     originalUrl: string,
     alias?: string
@@ -23,26 +29,30 @@ export class UrlShortenerServiceImpl implements UrlShortenerService {
       key = await this.getAvailableKey();
     }
 
-    await Url.query().insert({
-      id: key,
-      originalUrl,
-    });
+    await this.urlDao.insert(key, originalUrl);
     return this.constructShortenedUrl(key);
   }
 
   public async getOriginalUrl(urlKey: string): Promise<string> {
-    const urlInDb = await Url.query().findById(urlKey);
+    const cachedData = await this.redis.getString(urlKey);
+    // TODO: Test. Log cache hit or miss
+    if (cachedData) {
+      return cachedData;
+    }
+
+    const urlInDb = await this.urlDao.findById(urlKey);
     if (!urlInDb) {
       throwError({
         status: ErrorCode.NOT_FOUND,
         message: `/${urlKey} does not exist`,
       });
     }
+    await this.redis.setString(urlKey, urlInDb.originalUrl);
     return urlInDb.originalUrl;
   }
 
   private async ensureAliasDoesNotExist(alias: string): Promise<void> {
-    const url = await Url.query().findById(alias);
+    const url = await this.urlDao.findById(alias);
     if (url) {
       throwError({
         status: ErrorCode.CONFLICT,
@@ -50,12 +60,13 @@ export class UrlShortenerServiceImpl implements UrlShortenerService {
       });
     }
   }
+
   private async getAvailableKey(): Promise<string> {
     let result;
     let foundAvailableKey = false;
     do {
       const generatedKeys = this.keyGenerationService.generate();
-      const urlsInDb = await Url.query().findByIds(generatedKeys);
+      const urlsInDb = await this.urlDao.findByIds(generatedKeys);
       const keysUsed: { [key: string]: boolean } = {};
       for (const urlInDb of urlsInDb) {
         keysUsed[urlInDb.id] = true;
