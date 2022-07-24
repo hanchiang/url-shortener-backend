ac9405110fa79764858e0f6817e72140dc5ebcb5
han chiang <yaphc@hotmail.com>
2022-07-24 20:14:40 +0800

    Add event manager and event handler

--- src/services/impl/eventHandler.ts
+++ src/services/impl/eventHandler.ts
@@ -1,16 +1,55 @@
 import { RedirectUrlPayload, ShortenUrlPayload } from 'types/eventTypes';
 import { EventHandlerService } from '../eventHandler';
+import {
+  UrlRedirectStatsDao,
+  UrlRedirectStatsDaoImpl,
+} from '../../db/postgres/dao/urlRedirectStatsDao';
+import {
+  UrlShortenStatsDao,
+  UrlShortenStatsDaoImpl,
+} from '../../db/postgres/dao/urlShortenStatsDao';
 import logger from '../../utils/logger';
 
 // TODO: test
 export class EventHandlerServiceImpl implements EventHandlerService {
-  public handleRedirectUrl(payload: RedirectUrlPayload): Promise<void> {
-    logger.info(`handleRedirectUrl: `, { payload });
+  private urlRedirectStatsDao: UrlRedirectStatsDao;
+  private urlShortenStatsDao: UrlShortenStatsDao;
+
+  public constructor(
+    UrlRedirectStatsDao: UrlRedirectStatsDao,
+    urlShortenStatsDao: UrlShortenStatsDao
+  ) {
+    this.urlRedirectStatsDao = UrlRedirectStatsDao;
+    this.urlShortenStatsDao = urlShortenStatsDao;
+  }
+
+  public static defaultImpl(): EventHandlerService {
+    return new EventHandlerServiceImpl(
+      new UrlRedirectStatsDaoImpl(),
+      new UrlShortenStatsDaoImpl()
+    );
+  }
+
+  public async handleRedirectUrl(payload: RedirectUrlPayload): Promise<void> {
+    logger.info(`handleRedirectUrl event handler: `, { payload });
+    try {
+      await this.urlRedirectStatsDao.insert(payload.payload.hash);
+    } catch (e) {
+      logger.error('Encountered error when handling redirect url event: ', e);
+    }
     return;
   }
 
-  public handleShortenUrl(payload: ShortenUrlPayload): Promise<void> {
-    logger.info(`handleShortenUrl: `, { payload });
+  public async handleShortenUrl(payload: ShortenUrlPayload): Promise<void> {
+    logger.info(`handleShortenUrl event handler: `, { payload });
+    try {
+      await this.urlShortenStatsDao.insert(
+        payload.payload.originalUrl,
+        payload.payload.alias
+      );
+    } catch (e) {
+      logger.error('Encountered error when handling shorten url event: ', e);
+    }
     return;
   }
 }
--- src/services/impl/eventManager.ts
+++ src/services/impl/eventManager.ts
@@ -10,6 +10,7 @@ export class EventManagerServiceImpl implements EventManagerService {
   private static instance: EventManagerServiceImpl;
   private eventHandlerService: EventHandlerService;
   private eventEmitterService: EventEmitterService;
+
   private isRegistered: boolean;
 
   private constructor(
@@ -29,7 +30,7 @@ export class EventManagerServiceImpl implements EventManagerService {
 
   private static defaultImpl(): EventManagerServiceImpl {
     return new EventManagerServiceImpl(
-      new EventHandlerServiceImpl(),
+      EventHandlerServiceImpl.defaultImpl(),
       EventEmitterServiceImpl.getInstance()
     );
   }
@@ -39,33 +40,39 @@ export class EventManagerServiceImpl implements EventManagerService {
       logger.info('Event listeners are already registered');
       return;
     }
+
     this.eventEmitterService.on('error', async (error: any) => {
-      logger.error('Encountered error in event listener: ', { error });
+      logger.error('Encountered error in event listener: ', error);
     });
     this.eventEmitterService.on(
       'redirectUrl',
-      this.eventHandlerService.handleRedirectUrl
+      this.eventHandlerService.handleRedirectUrl.bind(this.eventHandlerService)
     );
     this.eventEmitterService.on(
       'shortenUrl',
-      this.eventHandlerService.handleShortenUrl
+      this.eventHandlerService.handleShortenUrl.bind(this.eventHandlerService)
     );
     this.isRegistered = true;
+    logger.info('Registered event listeners');
 
     return;
   }
 
   public deregisterListeners(): Promise<void> {
-    this.eventEmitterService.on('error', async (error: any) => {
+    this.eventEmitterService.removeListener('error', async (error: any) => {
       logger.error('Encountered error in event listener: ', { error });
     });
     this.eventEmitterService.removeListener(
       'redirectUrl',
-      this.eventHandlerService.handleRedirectUrl
+      this.eventHandlerService.handleRedirectUrl.bind(
+        this.eventHandlerService.handleRedirectUrl
+      )
     );
     this.eventEmitterService.removeListener(
       'shortenUrl',
-      this.eventHandlerService.handleShortenUrl
+      this.eventHandlerService.handleShortenUrl.bind(
+        this.eventHandlerService.handleShortenUrl
+      )
     );
     return;
   }
